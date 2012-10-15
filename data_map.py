9533177755bacc9c02bbbfe52b6133b28cd6602c
Songpon Phusing <songpon.ph@gmail.com>
2012-10-15 12:10:10 +0700

    init

---
+++ data_map.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import csv
+from pprint import pprint
+
+exist = csv.DictReader(file("exist.csv"))
+exist_data={}
+for e in exist:
+    exist_data[e['id']]=e['move_id']
+
+todo=[]
+to_update={}
+missing= csv.DictReader(file("missing.csv"))
+for m in missing:
+    move_id = exist_data.get(m['id'],False)
+    if move_id:
+        todo.append(move_id)
+        to_update[m['id']]=move_id
+
+print "SQL for delete"
+sql=" select id from account_move where id in (%s) "  %  ','.join(todo)
+print sql
+
+print "Sql for update"
+update = open("update.sql","w")
+for k,v in to_update.items():
+    update.write("update stock_picking set move_id=%s where id=%s;\n" %(v,k))
+update.close()
